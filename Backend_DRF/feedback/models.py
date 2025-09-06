ececab9b6790e91a1150d86a5c947174fa2220ff
Sunil Sapkota <sunil402sapkota@gmail.com>
2025-09-06 21:33:50 +0545

    feat: add created_at, phone, and rating fields to feedback model; create migration for changes

--- Backend_DRF/feedback/models.py
+++ Backend_DRF/feedback/models.py
@@ -1,10 +1,18 @@
 from django.db import models
+from django.core.validators import MinValueValidator,MaxValueValidator
 
 class feedbackModel(models.Model):
     name=models.CharField(max_length=100)
     email=models.CharField(max_length=100)
     subject=models.CharField(max_length=100)
     message=models.CharField(max_length=400)
+    rating=models.IntegerField(validators=[
+            MinValueValidator(0),
+            MaxValueValidator(5)
+            
+        ],blank=True,null=True)
+    phone=models.CharField(max_length=15,blank=True,null=True)
+    created_at=models.DateTimeField(auto_now_add=True,blank=True,null=True)
     
     
     
